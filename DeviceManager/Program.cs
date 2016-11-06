550a1bea6a5363e8762f2060be9b42b2efc53e28
Berend Wouters <berend.wouters@github.com>
2016-11-06 15:45:05 +0100

    Adds some management functionality for IoT

--- DeviceManager/Program.cs
+++ DeviceManager/Program.cs
@@ -7,8 +7,7 @@ namespace Brixel.Iot.DeviceManager
 {
     class Program
     {
-        private const string ConnectionString =
-            "";
+        private const string ConnectionString = "";
         static RegistryManager _registryManager;
         static void Main(string[] args)
         {
@@ -16,8 +15,29 @@ namespace Brixel.Iot.DeviceManager
             {
                 
                 _registryManager = RegistryManager.CreateFromConnectionString(ConnectionString);
-                AddDeviceAsync().Wait();
-                Console.WriteLine("Exited!");
+                System.Console.CancelKeyPress += (s, e) =>
+                {
+                    e.Cancel = true;
+                    Console.WriteLine("Exiting...");
+                };
+                var input = "";
+                while (String.Equals(input, String.Empty, StringComparison.Ordinal))
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("What do you want to do? Enter the number for the related action");
+                    Console.WriteLine("List devices: 1");
+                    Console.WriteLine("Create a device: 2");
+                    input = Console.ReadLine();
+                    if (input == "1")
+                    {
+                        ListDevices();
+                    }
+                    if (input == "2")
+                    {
+                        AddDeviceAsync().Wait();
+                    }
+                    input = "";
+                }
             }
             catch (AggregateException ex)
             {
@@ -32,13 +52,25 @@ namespace Brixel.Iot.DeviceManager
                 Console.WriteLine();
                 Console.WriteLine("Error in sample: {0}", ex.Message);
             }
-            Console.WriteLine("Press enter to exit...");
-            Console.ReadLine();
+        }
+
+        private static void ListDevices()
+        {
+            Console.WriteLine("------------------------" + Environment.NewLine +
+                              "Devices list for " + Environment.NewLine +
+                              "Brixel IoT Hub " + Environment.NewLine +
+                              "------------------------");
+            var listOfDevices = _registryManager.GetDevicesAsync(200).Result;
+            foreach (var device in listOfDevices)
+            {
+                Console.WriteLine($"Device {device.Id} is {device.ConnectionState}. It was last seen {device.LastActivityTime:dd-MM-yyyy HH:mm:ss}");
+            }
         }
 
         private static async Task<Device> AddDeviceAsync()
         {
-            string deviceId = "myFirstDevice";
+            Console.WriteLine("Please enter a device name");
+            string deviceId = Console.ReadLine();
             Device device;
             try
             {
@@ -46,6 +78,7 @@ namespace Brixel.Iot.DeviceManager
             }
             catch (DeviceAlreadyExistsException)
             {
+                Console.WriteLine("Device name already exists");
                 device = await _registryManager.GetDeviceAsync(deviceId);
             }
             Console.WriteLine("Generated device key: {0}", device.Authentication.SymmetricKey.PrimaryKey);
