219936fcb171c149f3182785a400d108e096eafb
Berend Wouters <berend.wouters@github.com>
2016-11-06 15:16:46 +0100

    Adds first version of the Brixel IoT Manager

---
+++ BrixelIoTClient/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Common.Exceptions;
+
+namespace Brixel.Iot.DeviceManager
+{
+    class Program
+    {
+        private const string ConnectionString =
+            "";
+        static RegistryManager _registryManager;
+        static void Main(string[] args)
+        {
+            try
+            {
+                
+                _registryManager = RegistryManager.CreateFromConnectionString(ConnectionString);
+                AddDeviceAsync().Wait();
+                Console.WriteLine("Exited!");
+            }
+            catch (AggregateException ex)
+            {
+                foreach (Exception exception in ex.InnerExceptions)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Error in sample: {0}", exception);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Error in sample: {0}", ex.Message);
+            }
+            Console.WriteLine("Press enter to exit...");
+            Console.ReadLine();
+        }
+
+        private static async Task<Device> AddDeviceAsync()
+        {
+            string deviceId = "myFirstDevice";
+            Device device;
+            try
+            {
+                device = await _registryManager.AddDeviceAsync(new Device(deviceId));
+            }
+            catch (DeviceAlreadyExistsException)
+            {
+                device = await _registryManager.GetDeviceAsync(deviceId);
+            }
+            Console.WriteLine("Generated device key: {0}", device.Authentication.SymmetricKey.PrimaryKey);
+            return device;
+            
+        }
+    }
+}
---
+++ SimulatedBrixelIoTDevice/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+
+namespace Brixel.IoT.SimulatedDevice
+{
+    class Program
+    {
+        static DeviceClient _deviceClient;
+        static string iotHubUri = "";
+        static string deviceKey = "";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Simulated device\n");
+            _deviceClient = DeviceClient.Create(iotHubUri, new DeviceAuthenticationWithRegistrySymmetricKey("myFirstDevice", deviceKey), TransportType.Mqtt);
+
+            SendDeviceToCloudMessagesAsync();
+            Console.ReadLine();
+        }
+
+        private static async void SendDeviceToCloudMessagesAsync()
+        {
+            double avgWindSpeed = 10; // m/s
+            Random rand = new Random();
+
+            while (true)
+            {
+                double currentWindSpeed = avgWindSpeed + rand.NextDouble() * 4 - 2;
+
+                var telemetryDataPoint = new
+                {
+                    deviceId = "myFirstDevice",
+                    windSpeed = currentWindSpeed
+                };
+                var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
+                var message = new Message(Encoding.ASCII.GetBytes(messageString));
+
+                await _deviceClient.SendEventAsync(message);
+                Console.WriteLine("{0} > Sending message: {1}", DateTime.Now, messageString);
+
+                Task.Delay(1000).Wait();
+            }
+        }
+    }
+}
